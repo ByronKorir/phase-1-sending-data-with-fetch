13a142b9d6e91cd19b11279ba50b351718f481df
byron korir <byron.korir@student.moringaschool.com>
2023-10-03 15:00:28 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Add your code here
+
+
+
+// //test 1(POST)
+function submitData(name,email){
+const request = {
+   name:"Steve",
+   email:"steve@steve.com"
+}
+fetch('http://localhost:3000/users',{
+   method:'POST',
+   headers:{
+      'Accept':"application/json",
+      'content-type':"application/json"
+   },
+   body:JSON.stringify(request)
+})
+
+.then((res) =>res.json())
+//handling the response
+.then((requestData)=>{   
+   const body = document.querySelector('body')
+   const newId = document.createElement('p')
+   newId.innerText=`newID: ${requestData.id}`
+   body.appendChild(newId)
+})  
+   //handling errors
+.catch((error)=>{
+   alert('an error occurred')
+})
+}
+
+
+submitData()
